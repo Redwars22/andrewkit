fafb8428e4aa4b9313bfc4f075a8ed8b00580442
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-04-19 13:04:04 -0300

    Add files via upload

---
+++ ui/constants.ts
@@ -0,0 +1,17 @@
+export const colors = {
+    "danger": "#fc0328",
+    "info": "#c8ccca",
+    "success": "#00b36b",
+    "normal": "#0d56bd",
+    "warning": "#FF8300"
+}
+
+export const typographySizes = {
+    h1: '2.5rem',
+    h2: '2rem',
+    h3: '1.75rem',
+    h4: '1.5rem',
+    h5: '1.25rem',
+    h6: '1rem',
+    paragraph: '1rem',
+  };
